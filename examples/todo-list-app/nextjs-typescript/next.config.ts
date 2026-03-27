13768b9f46295b3c710f210c7892c1dbb1961802
venkateshsundaram <contactvenkateshsundaram@gmail.com>
2026-03-28 00:43:53 +0530

    fix: adding support dev origin for nextjs projects with suppress hydration warning

--- examples/todo-list-app/nextjs-typescript/next.config.ts
+++ examples/todo-list-app/nextjs-typescript/next.config.ts
@@ -1,17 +1,9 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const currFilename = fileURLToPath(import.meta.url);
-const currDirname = path.dirname(currFilename);
 
 const nextConfig: NextConfig = {
   /* config options here */
   // CodeSandbox dev origin
   allowedDevOrigins: ["*.csb.app"],
-  turbopack: {
-    root: currDirname,
-  },
 };
 
 export default nextConfig;
