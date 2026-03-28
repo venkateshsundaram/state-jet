d4b11eb2683399166786187e7c5245c0434c4f84
venkateshsundaram <contactvenkateshsundaram@gmail.com>
2026-03-28 12:21:48 +0530

    fix: adding support dev origin for nextjs projects with suppress hydration warning

--- examples/todo-list-app/nextjs-javascript/next.config.mjs
+++ examples/todo-list-app/nextjs-javascript/next.config.mjs
@@ -5,18 +5,6 @@ const nextConfig = {
   /* config options here */
   // CodeSandbox dev origin
   allowedDevOrigins: ["*.csb.app"],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "tailwindcss": path.resolve(process.cwd(), "node_modules/tailwindcss"),
-    };
-    return config;
-  },
-  turbopack: {
-    resolveAlias: {
-      "tailwindcss": path.resolve(process.cwd(), "node_modules/tailwindcss"),
-    },
-  },
 };
 
 export default nextConfig;
