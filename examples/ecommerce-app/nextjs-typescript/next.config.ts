89dc56850f95223a6627cd55b07f8a05560bcfc7
venkateshsundaram <contactvenkateshsundaram@gmail.com>
2026-03-28 16:53:25 +0530

    fix: nextjs example project refactor

--- examples/ecommerce-app/nextjs-typescript/next.config.ts
+++ examples/ecommerce-app/nextjs-typescript/next.config.ts
@@ -1,30 +1,9 @@
 import type { NextConfig } from "next";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __DIRNAME = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   /* config options here */
   // CodeSandbox dev origin
   allowedDevOrigins: ["*.csb.app"],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      tailwindcss: path.resolve(__DIRNAME, "node_modules/tailwindcss"),
-    };
-    config.resolve.modules = [
-      path.resolve(__DIRNAME, "node_modules"),
-      "node_modules",
-      ...(config.resolve.modules || []),
-    ];
-    return config;
-  },
-  turbopack: {
-    resolveAlias: {
-      tailwindcss: path.resolve(__DIRNAME, "node_modules/tailwindcss"),
-    },
-  },
 };
 
 export default nextConfig;
